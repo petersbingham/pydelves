642a8912d7fae6e441588e69b9c426cf8edd8d14
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 11:05:43 +0000

    Copy test file and README on installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import shutil
+shutil.copy('README.md', 'pyDelves/README.md')
 
 setup(name='pyDelves',
-      version='2.4',
+      version='2.5',
       description='Finds roots of an analytical function',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
       packages=['pydelves'],
+      package_data={'pydelves': ['tests/*', 'README.md'],
      )
